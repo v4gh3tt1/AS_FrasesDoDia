c7a828a2baea0a3db8881b924e0a327b710aa11d
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-11-24 11:38:06 -0300

    Criado Array de frases
    As frases são exibidas no textView de forma aleatoria

--- app/src/main/java/com/example/as_frasesdodia/MainActivity.java
+++ app/src/main/java/com/example/as_frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.as_frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View view){
+
+        String [] frases = {
+                "frase1",
+                "frase2",
+                "frase3",
+                "frase4" // nao precisa virgula no ultimo item
+        };
+
+        int numero = new Random().nextInt(4); // gera numeros aleatorios de 0 a 4
+
+        TextView texto = findViewById(R.id.textResultado); // atribui resultado ao TextView id 'textResultado'
+        texto.setText(frases[numero]); // indice aleatorio do array 'frases[]' exibido no TextView
+
+    }
+
 }
